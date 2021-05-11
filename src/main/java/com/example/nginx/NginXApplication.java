74cf2a768499da583ee04dd69daebc3f2ba9cec9
NamSeok_DT <skatjr30@naver.com>
2021-05-11 23:05:29 +0900

    init NginX

---
+++ src/main/java/com/example/nginx/NginXApplication.java
@@ -0,0 +1,13 @@
+package com.example.nginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NginXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nginx/NginXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
