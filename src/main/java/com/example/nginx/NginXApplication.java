f749d35c52ee87ad2d4cc0f3de33106fe3dbc67d
NamSeok_DT <skatjr30@naver.com>
2021-05-12 03:26:08 +0900

    feat : add WebRestController

--- src/main/java/com/example/nginx/NginXApplication.java
+++ src/main/java/com/example/nginx/NginXApplication.java
@@ -2,19 +2,12 @@ package com.example.nginx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NginXApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "/app/config/NginX/real-application.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(NginXApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(NginXApplication.class, args);
     }
 
 }
