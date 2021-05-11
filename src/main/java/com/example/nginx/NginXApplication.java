ae6735de471cbf39c3755869b1612ce9fb1a45a5
NamSeok_DT <skatjr30@naver.com>
2021-05-12 02:44:06 +0900

    feat : main add

--- src/main/java/com/example/nginx/NginXApplication.java
+++ src/main/java/com/example/nginx/NginXApplication.java
@@ -1,13 +1,19 @@
 package com.example.nginx;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NginXApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(NginXApplication.class, args);
+        new SpringApplicationBuilder(NginXApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
