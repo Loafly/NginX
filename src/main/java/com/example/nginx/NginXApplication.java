94d67e23a9e7da85eafda78236ffee0fc8c2c6b6
NamSeok_DT <skatjr30@naver.com>
2021-05-12 02:52:36 +0900

    chore : main

--- src/main/java/com/example/nginx/NginXApplication.java
+++ src/main/java/com/example/nginx/NginXApplication.java
@@ -8,7 +8,7 @@ public class NginXApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
-            + "/app/config/springboot-webservice/real-application.yml";
+            + "/app/config/NginX/real-application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(NginXApplication.class)
